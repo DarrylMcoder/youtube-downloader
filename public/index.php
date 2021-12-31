8512e1b9fff043a15ebb4c6863e48b4b68369dbc
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-31 16:51:48 -0500

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -15,6 +15,19 @@
     }
     
   </style>
+  
+  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FM6872BBGN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FM6872BBGN');
+</script>
+  
+  
 </head>
 <body>
   <?php  echo file_get_contents('http://static.darrylmcoder.epizy.com/assets/header.html'); ?>
