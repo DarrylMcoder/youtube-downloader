0767ccb539d47e2912d21ff1aa131c04f4e88d4a
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-01-10 21:31:54 -0500

    Add files via upload

--- public/login.php
+++ public/login.php
@@ -123,7 +123,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
-        <form action="?next=<?php echo $_GET["next"]; ?>" method="post">
+        <form action="?next=<?php echo isset($_GET['next']) ? $_GET["next"] : ''; ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
