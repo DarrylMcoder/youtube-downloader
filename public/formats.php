806de5a76be75ee7f78f45a166a268a33fd7a313
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-01-31 21:02:57 -0500

    Add files via upload

--- public/formats.php
+++ public/formats.php
@@ -3,14 +3,6 @@
 ini_set('error_reporting', E_ALL ^ E_NOTICE); 
 ini_set('display_errors', 1); 
 
-session_start();  // Initialize the session
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-
 set_time_limit(0);
 
 require('../vendor/autoload.php');
